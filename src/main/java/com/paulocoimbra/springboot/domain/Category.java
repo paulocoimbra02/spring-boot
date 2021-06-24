147ba411888be03ca263c2d1a63fba1ae5636e83
Paulo Matheus Coimbra <paulomatheusc@hotmail.com>
2021-06-23 22:30:02 -0300

    Working H2

--- src/main/java/com/paulocoimbra/springboot/domain/Category.java
+++ src/main/java/com/paulocoimbra/springboot/domain/Category.java
@@ -1,11 +1,19 @@
 package com.paulocoimbra.springboot.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
 public class Category implements Serializable {
 
     private static final long serialVersionUID = 144400346356575925L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String name;
 
