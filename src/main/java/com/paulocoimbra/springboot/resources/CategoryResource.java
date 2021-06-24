5f991ef4063ad76aac8c79ee962ed1b63ea51755
Paulo Matheus Coimbra <paulomatheusc@hotmail.com>
2021-06-23 21:54:11 -0300

    Testing first domain class

--- src/main/java/com/paulocoimbra/springboot/resources/CategoryResource.java
+++ src/main/java/com/paulocoimbra/springboot/resources/CategoryResource.java
@@ -1,15 +1,26 @@
 package com.paulocoimbra.springboot.resources;
 
+import com.paulocoimbra.springboot.domain.Category;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/categories")
 public class CategoryResource {
 
 	@RequestMapping(method = RequestMethod.GET)
-	public String list() {
-		return "working rest";
+	public List<Category> list() {
+		Category cat1 = new Category(1, "Informática");
+		Category cat2 = new Category(2, "Escritório");
+
+		List<Category> list = new ArrayList<>();
+		list.add(cat1);
+		list.add(cat2);
+
+		return list;
 	}
 }
