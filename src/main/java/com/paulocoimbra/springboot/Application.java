43b72b3102c2c6ba180026cda92f2eadd56a80c5
Paulo Matheus Coimbra <paulomatheusc@hotmail.com>
2021-07-10 02:37:37 -0300

    Implementing Upload of file to AWS S3

--- src/main/java/com/paulocoimbra/springboot/Application.java
+++ src/main/java/com/paulocoimbra/springboot/Application.java
@@ -1,5 +1,7 @@
 package com.paulocoimbra.springboot;
 
+import com.paulocoimbra.springboot.service.S3Service;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,12 +9,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application implements CommandLineRunner {
 
+    @Autowired
+    private S3Service s3Service;
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
 
     @Override
     public void run(String... args) throws Exception {
-
+        s3Service.uploadFile("C:\\temp\\fotos\\statistics.png");
     }
 }
